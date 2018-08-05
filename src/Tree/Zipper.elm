dd23e6a767fc4a9c19befd49ca42c035d89328f3
ilias <mail@ilias.xyz>
2018-08-05 13:07:49 +0200

    019 prep

--- src/Tree/Zipper.elm
+++ src/Tree/Zipper.elm
@@ -670,18 +670,18 @@ nextSiblingOfAncestor zipper =
         Nothing ->
             Nothing
 
-        Just parent ->
-            case nextSibling parent of
+        Just parent_ ->
+            case nextSibling parent_ of
                 Nothing ->
-                    nextSiblingOfAncestor parent
+                    nextSiblingOfAncestor parent_
 
                 Just s ->
                     Just s
 
 
 reconstruct : Tree a -> Crumb a -> Tree a
-reconstruct focus { before, label, after } =
-    Tree.tree label (List.reverse before ++ [ focus ] ++ after)
+reconstruct focus c =
+    Tree.tree c.label (List.reverse c.before ++ [ focus ] ++ c.after)
 
 
 reconstructWithoutFocus : Crumb a -> Tree a
